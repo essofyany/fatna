d07365a40b5d0ad5df77761d660d4aa0c693c517
Bilal Essofyany <soufyani.bilal@gmail.com>
2021-06-17 21:34:20 +0100

    adding route for updating current user password

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,27 @@
 import User from "../models/user.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import { sendToken } from "../utils/jwtToken.js";
+import encryptor from "../utils/encryptor.js";
+
+// Update user password => api/user/update/password
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  //   check previous password
+  const isMatched = await user.comparePasswords(req.body.oldPassword);
+
+  if (!isMatched) {
+    return next(new ErrorHandler("Old password is incorrect", 400));
+  }
+
+  const hashedPassword = await encryptor(req.body.password);
+
+  user.password = hashedPassword;
+  await user.save();
+
+  sendToken(user, 200, res);
+});
 
 // Get currrent user profile => api/user/current
 export const getCurrentUser = catchAsyncErrors(async (req, res, next) => {
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
-import { getCurrentUser } from "../controllers/user.js";
+import { getCurrentUser, updatePassword } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/current", isAuthenticatedUser, getCurrentUser);
+
+router.put("/update/password", isAuthenticatedUser, updatePassword);
 
 export default router;
