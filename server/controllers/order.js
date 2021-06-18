c1b3ab427a4870f6c9579d7aa49823bfeac93b93
Bilal Essofyany <soufyani.bilal@gmail.com>
2021-06-18 18:49:57 +0100

    adding ADMIN routes for Getting all Orders- and Updating Order Status (processing <--> delivered)- and Deleting order

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -50,7 +50,6 @@ export const getSingleOrder = catchAsyncErrors(async (req, res, next) => {
 });
 
 // Get Current User Orders => api/order/myOrders
-
 export const getCurrentUserOrders = catchAsyncErrors(async (req, res, next) => {
   const orders = await Order.find({
     user: req.user.id,
@@ -63,6 +62,69 @@ export const getCurrentUserOrders = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
+  });
+});
+
+// Get All Orders - ADMIN=> api/order/admin
+export const getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totlaAmount = 0;
+  orders.forEach((order) => {
+    totlaAmount += order.totalPrice;
+  });
+
+  if (!orders) return next(new ErrorHandler("There are no orders", 404));
+
+  res.status(200).json({
+    success: true,
+    totlaAmount,
     orders,
+  });
+});
+
+// Update order status processing to delivered - ADMIN=> api/order/admin/:id
+export const updateOrderStatus = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order)
+    return next(new ErrorHandler("There is no order with this ID", 404));
+
+  if (order.orderStatus === "Delivered")
+    return next(new ErrorHandler("You have already delivered this order", 400));
+
+  order.orderItems.forEach(async (item) => {
+    await updateStock(item.product, item.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock -= quantity;
+
+  await product.save();
+}
+
+// Delete Order - ADMIN=> api/order/admin/:id
+export const deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order)
+    return next(new ErrorHandler("There is no order with this ID", 404));
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
   });
 });
--- server/routes/order.js
+++ server/routes/order.js
@@ -3,6 +3,9 @@ import {
   createOrder,
   getSingleOrder,
   getCurrentUserOrders,
+  getAllOrders,
+  updateOrderStatus,
+  deleteOrder,
 } from "../controllers/order.js";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 
@@ -10,7 +13,28 @@ const router = express.Router();
 
 router.post("/", isAuthenticatedUser, createOrder);
 
+router.get(
+  // admin route
+  "/admin",
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  getAllOrders
+);
+router.put(
+  // admin route
+  "/admin/:id",
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  updateOrderStatus
+);
+router.delete(
+  // admin route
+  "/admin/:id",
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  deleteOrder
+);
+
 router.get("/myOrders", isAuthenticatedUser, getCurrentUserOrders);
 router.get("/:id", isAuthenticatedUser, getSingleOrder);
-
 export default router;
